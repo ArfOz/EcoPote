7cb3187c72138bfed34f3dc7c486bc195fd36e8d
Arif Trends <arfoz1245@gmail.com>
2024-12-02 11:07:19 +0100

    changed base

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome frontend 👋
+              Welcome frontend ssss👋
             </h1>
           </div>
 
