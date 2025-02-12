26f805eaacbf6701ac057c6a62bf77a786814419
Arif Trends <arfoz1245@gmail.com>
2025-02-12 22:36:06 +0100

    feat: add Cron model to database schema and update CronController with cron time format documentation

--- apps/backend/src/cron/cron.controller.ts
+++ apps/backend/src/cron/cron.controller.ts
@@ -28,3 +28,13 @@ export class CronController {
     }
   }
 }
+
+// *    *    *    *    *    *
+// ┬    ┬    ┬    ┬    ┬    ┬
+// │    │    │    │    │    │
+// │    │    │    │    │    └─ Day of the week (0 - 7) (Sunday is both 0 and 7)
+// │    │    │    │    └───── Month (1 - 12)
+// │    │    │    └────────── Day of the month (1 - 31)
+// │    │    └─────────────── Hour (0 - 23)
+// │    └──────────────────── Minute (0 - 59)
+// └───────────────────────── Second (0 - 59, optional)
