d9638a91d70ce6ccb8d409c4279920044488a204
Arif Trends <arfoz1245@gmail.com>
2025-03-09 23:51:23 +0100

    feat: enhance CronTime component with editable job names and improved update handling

--- apps/frontend/src/app/components/CronTime.tsx
+++ apps/frontend/src/app/components/CronTime.tsx
@@ -1,6 +1,4 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { fetchWithAuth } from '@utils';
 import { useRouter } from 'next/navigation';
 import { ResponseCron, ResponseCronUpdateDto, ScheduleEnum } from '@shared/dtos';
@@ -8,25 +6,20 @@ import { ResponseCron, ResponseCronUpdateDto, ScheduleEnum } from '@shared/dtos'
 export const CronTime = () => {
   const [data, setData] = useState<ResponseCron['data']>([]);
   const [error, setError] = useState<string | null>(null);
+  const [editingId, setEditingId] = useState<number | null>(null);
+  const originalNameRef = useRef<{ [key: number]: string }>({}); // Store original names
 
   const router = useRouter();
 
   useEffect(() => {
     const fetchCronJobs = async () => {
       try {
-        const res: ResponseCron = await fetchWithAuth(
-          'cron/get-jobs',
-          {},
-          false
-        );
+        const res: ResponseCron = await fetchWithAuth('cron/get-jobs', {}, false);
         setData(res.data);
       } catch (error) {
         if (error instanceof Error) {
           setError(error.message);
-          if (
-            error.message === 'Token is expired' ||
-            error.message === 'Unauthorized access'
-          ) {
+          if (error.message === 'Token is expired' || error.message === 'Unauthorized access') {
             localStorage.removeItem('token');
             router.push('/login');
           }
@@ -39,66 +32,30 @@ export const CronTime = () => {
     fetchCronJobs();
   }, [router]);
 
-  const handleChange = async ({id, status, schedule}:{id: number, status?: boolean, schedule?: string}) => {
+  const handleChange = async ({ id, name, schedule, status }: { id: number; name?: string; schedule?: string; status?: boolean }) => {
     try {
 
-      const body :{ id: number; status?: boolean; schedule?: string } = {
-        id,
-        ...(status !== undefined && { status }),
-        ...(schedule !== undefined && { schedule }),
-      };
-      const response: ResponseCronUpdateDto = await fetchWithAuth(
-        'cron/update-job',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(body),
-        }
-      );
-
-      console.log("response", response);
+      console.log('Updating cron job:', { id, name, schedule, status });
+      const body = { id, ...(name !== undefined && { name }), ...(schedule !== undefined && { schedule }), ...(status !== undefined && { status }) };
+      const response: ResponseCronUpdateDto = await fetchWithAuth('cron/update-job', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
 
       if (response.success) {
-        // Update the local state with the new status or schedule
-        
-        setData([response.data]);
-
+        setData((prevData) => prevData.map((job) => (job.id === id ? response.data : job)));
+        setEditingId(null);
         console.log('Cron job updated successfully');
       } else {
         console.error('Failed to update cron job', response.message);
       }
     } catch (error) {
       console.error('Failed to update cron job', error);
-    } 
+    }
   };
-
-  // // Function to handle schedule change
-  // const handleScheduleChange = async (id: number, schedule: string) => {
-  //   try {
-  //     const response: ResponseCronUpdateDto = await fetchWithAuth(
-  //       'cron/update-job',
-  //       {
-  //         method: 'POST',
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //         body: JSON.stringify({ id, schedule }),
-  //       }
-  //     );
-  //     if (response.success) {
-  //       setData((prevData) =>
-  //         prevData.map((job) => (job.id === id ? { ...job, schedule } : job))
-  //       );
-  //       console.log('Schedule updated successfully');
-  //     } else {
-  //       console.error('Failed to update schedule', response.message);
-  //     }
-  //   } catch (error) {
-  //     console.error('Failed to update schedule', error);
-  //   }
-  // };
 
   return (
     <div className="bg-gray-100 p-4 rounded shadow-md">
@@ -110,10 +67,9 @@ export const CronTime = () => {
             <th className="py-2 px-4 border-b">ID</th>
             <th className="py-2 px-4 border-b">Name</th>
             <th className="py-2 px-4 border-b">Schedule</th>
-            {/* <th className="py-2 px-4 border-b">Time</th> */}
             <th className="py-2 px-4 border-b">Created</th>
             <th className="py-2 px-4 border-b">Updated</th>
-
+            <th className="py-2 px-4 border-b">Start Time</th>
             <th className="py-2 px-4 border-b">Status</th>
             <th className="py-2 px-4 border-b">Last Run</th>
             <th className="py-2 px-4 border-b">Next Run</th>
@@ -123,33 +79,67 @@ export const CronTime = () => {
           {data.map((cronJob) => (
             <tr key={cronJob.id}>
               <td className="py-2 px-4 border-b">{cronJob.id}</td>
-              <td className="py-2 px-4 border-b">{cronJob.name}</td>
+              <td className="py-2 px-4 border-b">
+                <input
+                  type="text"
+                  className="bg-transparent border-none"
+                  value={cronJob.name}
+                  onFocus={() => {
+                    setEditingId(cronJob.id);
+                    originalNameRef.current[cronJob.id] = cronJob.name; // Store original name
+                  }}
+                  onChange={(e) => {
+                    setData((prevData) =>
+                      prevData.map((job) =>
+                        job.id === cronJob.id ? { ...job, name: e.target.value } : job
+                      )
+                    );
+                  }}
+                />
+                {editingId === cronJob.id && (
+                  <div className="flex space-x-2 mt-2">
+                    <button
+                      className="px-2 py-1 bg-green-500 text-white rounded"
+                      onClick={() => handleChange({ id: cronJob.id, name: cronJob.name })}
+                    >
+                      Save
+                    </button>
+                    <button
+                      className="px-2 py-1 bg-red-500 text-white rounded"
+                      onClick={() => {
+                        setData((prevData) =>
+                          prevData.map((job) =>
+                            job.id === cronJob.id
+                              ? { ...job, name: originalNameRef.current[cronJob.id] }
+                              : job
+                          )
+                        );
+                        setEditingId(null);
+                      }}
+                    >
+                      Discard
+                    </button>
+                  </div>
+                )}
+              </td>
               <td className="py-2 px-4 border-b">
                 <select
                   className="ml-2 px-2 py-1 bg-gray-200 rounded"
                   value={cronJob.schedule}
-                
-                  onChange={
-                    (e) =>
-                      handleChange({id:cronJob.id,schedule: e.target.value})
-                  }
+                  onChange={(e) => handleChange({ id: cronJob.id, schedule: e.target.value })}
                 >
                   {Object.values(ScheduleEnum).map((schedule) => (
                     <option key={schedule} value={schedule}>
                       {schedule.replace(/-/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase())}
                     </option>
                   ))}
-              
                 </select>
               </td>
-              {/* <td className="py-2 px-4 border-b">{cronJob.cronTime}</td> */}
+              <td className="py-2 px-4 border-b">{new Date(cronJob.createdAt).toLocaleString()}</td>
+              <td className="py-2 px-4 border-b">{new Date(cronJob.updatedAt).toLocaleString()}</td>
+              <td className="py-2 px-4 border-b">{new Date(cronJob.startTime).toLocaleString()}</td>
               <td className="py-2 px-4 border-b">
-                {new Date(cronJob.startTime).toLocaleString()}
-              </td>
-              <td className="py-2 px-4 border-b">
-                {new Date(cronJob.updatedAt).toLocaleString()}
-              </td>
-              <td className="py-2 px-4 border-b">
+             
                 <div className="flex flex-col items-center">
                   <div>
                     <span
@@ -157,14 +147,14 @@ export const CronTime = () => {
                         cronJob.status ? 'bg-green-500' : 'bg-red-500'
                       }`}
                     ></span>
-                    <span className="ml-2">
+                     <span className="ml-2">
                       {cronJob.status ? 'Active' : 'Inactive'}
                     </span>
                   </div>
                   <button
                     className="ml-2 px-2 py-1 bg-blue-500 text-white rounded"
                     onClick={() =>
-                      handleChange({id:cronJob.id, status:cronJob.status})
+                      handleChange({id:cronJob.id, status:!cronJob.status})
                     }
                   >
                     {cronJob.status ? 'Deactivate' : 'Activate'}
@@ -172,9 +162,7 @@ export const CronTime = () => {
                 </div>
               </td>
               <td className="py-2 px-4 border-b">
-                {cronJob.lastRun
-                  ? new Date(cronJob.lastRun).toLocaleString()
-                  : 'N/A'}
+                {cronJob.lastRun ? new Date(cronJob.lastRun).toLocaleString() : 'N/A'}
               </td>
               <td className="py-2 px-4 border-b">N/A</td>
             </tr>
